5a0e7c9beeeabb4ee7220e240f7ce05f5cbc083b
Kyle Pace <kylelpace@gmail.com>
2020-11-05 22:39:58 -0700

    adding datatable

--- employee-directory/src/components/SearchResults/index.js
+++ employee-directory/src/components/SearchResults/index.js
@@ -1,40 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./style.css";
+// import { storiesOf } from '@storybook/react';
+// import data from '../constants/sampleMovieData';
+import DataTable, { createTheme } from 'react-data-table-component';
+import API from "../../utils/API";
 
-function SearchResults(props) {
+function SearchResults(props)
+ { 
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+     API.getRandomEmployees().then(employees => {
+       console.log(employees.data.results);
+       setData(employees.data.results)
+     })
+   },[])
+  const columns = [
+    {
+      name: 'Name',
+      selector: row => row.name.first + " " + row.name.last,
+      sortable: true,
+    },
+    {
+      name: 'Email',
+      selector: 'email',
+      sortable: true,
+    },
+    {
+      name: 'Gender',
+      selector: 'gender',
+      sortable: true,
+    },
+  ];
   return (
-    <table class="table table-striped table-inverse table-responsive">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Image</th>
-          <th scope="col">Last Name</th>
-          <th scope="col">First Name</th>
-          <th scope="col">
-            Cell <i class="fas fa-phone"></i>
-          </th>
-          <th scope="col">
-            Email <i class="far fa-envelope"></i>
-          </th>
-        </tr>
-      </thead>
+    
+  
+      <DataTable
+        title="Employee"
+        columns={columns}
+        data={data}
+        defaultSortField="title"
+      />
+    )
+    
+   
+};
 
-      <tbody>
-        {props.results.map((employee, index) => (
-          <tr key={employee.name.last}>
-            <th scope="row"></th>
-            <td>
-              <img src={employee.picture.thumbnail} alt="Employee Image"></img>{" "}
-            </td>
-            <td> {employee.name.last} </td>
-            <td> {employee.name.first} </td>
-            <td> {employee.cell} </td>
-            <td> {employee.email} </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
 
 export default SearchResults;
